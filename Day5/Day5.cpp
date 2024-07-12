d1ebfb5d85ff1cacc4dba66ac96590b844d49f3d
AMANDEEP SINGH <2021a1r168@mietjammu.in>
2024-07-12 19:41:09 +0530

    committed

--- Day5/Day5.cpp
+++ Day5/Day5.cpp
@@ -84,3 +84,80 @@
 
 //     return 0;
 // }
+
+// _____________________________________________________________________________________________
+// Write a Program such that dosenot allow to make object
+
+// #include<iostream>
+// using namespace std;
+// class A{
+//     private:
+//         int val;
+//         A();
+// };
+// A::A(){
+//     cin>>val;
+// }
+// int main(){
+//     A obj;
+//     return 0;
+// }
+
+// _____________________________________________________________________________________________
+// Write a Program such that only one object is allowed.
+
+#include<iostream>
+using namespace std;
+class A{
+    private:
+        A();
+    public:
+        int x;
+        static int val;
+};
+int A::val=0;
+A::A(){
+    exit(0);
+    // if (val==0){
+    //     val++;
+    // }
+    // else{
+    //     __throw_runtime_error("Can't Create Objects");
+    // }
+
+}
+int main(){
+    A obj1,obj2,obj3,obj4;
+    obj1.x=100;
+    obj2.x=200;
+    obj3.x=300;
+    obj4.x=400;
+    cout<<obj1.x;
+    cout<<obj2.x;
+    cout<<obj3.x;
+    cout<<obj4.x;
+    return 0;
+}
+-
+
+// _____________________________________________________________________________________________
+// WAP to count how many objects of a class is created.
+
+
+// #include<iostream>
+// using namespace std;
+// class A{
+//     public:
+//         static int val;
+//         A();
+// };
+// int A::val=0;
+// A::A(){
+//     val++;
+
+// }
+// int main(){
+//     A obj1,obj2,obj3,obj4;
+//     cout<<obj3.val;
+//     return 0;
+// }
